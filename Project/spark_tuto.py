107fbcc8679879e9145c1712fd8ac8dbfff46b20
Pierre Armel <pierre.mbangandjock@epfl.ch>
2017-11-24 20:42:59 +0100

    article

--- Project/spark_tuto.py
+++
@@ -1,4 +0,0 @@
-from pyspark import *
-
-textFile = spark.read.text("/home/mbanga/README.txt")
-textFile.count()
--- Project/test.py
+++
@@ -1,3 +0,0 @@
-from pyspark import SparkContext
-
-RDDread = sc.textFile ("/home/mbanga/Epfl/AppliedDataAnalysis/ADA2017_GroupWork/Project/CHANGES.txt")
